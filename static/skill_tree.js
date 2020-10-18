bfa4d0967f76a5251893d0fc2f46111b3ba3e540
peter <ineveraskedforthis@users.noreply.github.com>
2020-10-18 20:09:34 +0300

    tooltip strings for skills

--- static/skill_tree.js
+++ static/skill_tree.js
@@ -1,3 +1,24 @@
+SKILL_DESC = {
+    'warrior_training': 'improve your physical conditions, at final level gives you a tactic slot 2',
+    'mage_training': 'improve your magical power, at final level gives you a tactic slot 2',
+    'charge': 'unlock charge ability: you are moved to your enemy position at cost of increasing rage',
+    'rage_control': 'rage influence accuracy less',
+    'cold_rage': 'rage influence accuracy even less',
+    'the_way_of_rage': 'rage almost does not influence accuracy',
+    'blocking_movements': 'you learn basics of blocking attacks, now you have better chances to block enemy attack',
+    'blood_battery': 'blood now makes all magical damage higher',
+    'first_steps_in_magic': 'unlock magic bolt: ranged ability with low blunt damage',
+    'less_stress_from_crafting': 'at second level of this ability you can safely craft various things, third one decrease stress even more and unlock futher development of your crafting abilities',
+    'less_stress_from_making_food': 'you gain less stress from preparing food',
+    'disenchanting': 'unlock ability to break items into zaz',
+    'less_stress_from_disenchant': 'you gain less stress from disenchanting',
+    'sewing': 'unlock ability to sew clothes',
+    'cook': 'unlock ability to prepare food',
+    'enchanting':'unlock ability to use zaz to add enchantments to items',
+    'less_stress_from_enchant': 'you gain less stress from enchanting',
+}
+
+
 function send_skill_up_message(socket, tag) {
     console.log(tag)
     socket.emit('up-skill', tag);
