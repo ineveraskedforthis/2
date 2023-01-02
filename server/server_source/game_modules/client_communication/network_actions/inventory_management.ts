30dac3a22aedbade55aa201273a99b5eb37581af
ineveraskedforthis <ineveraskedforthis@users.noreply.github.com>
2023-01-03 02:16:17 +0300

    connect bulk orders to frontend

--- server/server_source/game_modules/client_communication/network_actions/inventory_management.ts
+++ server/server_source/game_modules/client_communication/network_actions/inventory_management.ts
@@ -20,7 +20,7 @@ function r(f: (user: User, character: Character) => void): (sw: SocketWrapper) =
 
 export namespace InventoryCommands {
     export function equip(sw: SocketWrapper, msg: number) {
-        console.log('equip command + ' + msg)
+        // console.log('equip command + ' + msg)
         const [user, character] = Convert.socket_wrapper_to_user_character(sw)
         if (character == undefined) return
         if (character.in_battle()) { return }
@@ -43,7 +43,7 @@ export namespace InventoryCommands {
         if (character == undefined) return
         if (character.in_battle()) { return }
 
-        console.log('unequip ' + msg)
+        // console.log('unequip ' + msg)
 
         if (msg == "weapon")    {EventInventory.unequip(character, 'weapon')}
         else if (msg == 'secondary'){EventInventory.unequip_secondary(character)}
