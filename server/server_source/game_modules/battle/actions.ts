ea380de82946af6fb4d117cf9701bb3723959be7
ineveraskedforthis <ineveraskedforthis@users.noreply.github.com>
2023-01-19 08:41:47 +0300

    start implementation of unified battle actions list on backend

---
+++ server/server_source/game_modules/battle/actions.ts
@@ -0,0 +1,22 @@
+import { Character } from "../character/character";
+import { CharacterSystem } from "../character/system";
+import { Unit } from "./classes/unit";
+
+interface BattleActionServer {
+    ap_cost: (character: Character, unit: Unit, distance: number) => number
+    unlocked: (character: Character) => boolean
+}
+
+function always(character: Character): boolean {
+    return true
+}
+
+const ActionsList: {[_ in string]: BattleActionServer} = {
+    'move' : {
+        ap_cost: (character: Character, unit: Unit, distance: number) => {
+            return CharacterSystem.movement_cost_battle(character) * distance
+        },
+
+        unlocked: always
+    }
+}
