343aa7a8296e9cb74f6c888f213f5259e3bd2451
ineveraskedforthis <ineveraskedforthis@users.noreply.github.com>
2024-10-21 17:00:03 +0300

    adjust script

--- static/img/character_image/renamer.py
+++ static/img/character_image/renamer.py
@@ -9,16 +9,11 @@ def main():
     fix files extension
     """
     path = "./static/img/character_image/"
-    count = 1
-
     for root, dirs, files in os.walk(path):
         for i in files:
             if i.endswith('.png'):
-                result = i[:-4]
-                print(result + ".PNG")
-                # os.rename(os.path.join(root, i), os.path.join(root, "changed" + str(count) + ".txt"))
-            count += 1
-
+                result = i[:-4] + ".PNG"
+                os.rename(os.path.join(root, i), os.path.join(root, result))
 
 if __name__ == '__main__':
     main()
