77f4579031c6025b2c029e3ececf3dd271b9461b
ineveraskedforthis <ineveraskedforthis@users.noreply.github.com>
2024-10-21 18:21:06 +0300

    minor python things

--- static/img/character_image/renamer.py
+++ static/img/character_image/renamer.py
@@ -9,7 +9,7 @@ def main():
     fix files extension
     """
     path = "./static/img/character_image/"
-    for root, dirs, files in os.walk(path):
+    for root, _, files in os.walk(path):
         for i in files:
             if i.endswith('.png'):
                 result = i[:-4] + ".PNG"
